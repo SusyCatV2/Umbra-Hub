6a3272fe7df3359cf9cb3e98666be5ec7e2b1abb
Dawg <156109947+SusyCatV2@users.noreply.github.com>
2025-11-25 18:19:42 +0200

    Refactor loading of Required.lua in loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/SusyCatV2/Umbra-Hub/refs/heads/main/Required.lua"))
+if not _G.umbraLoaded then loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/SusyCatV2/Umbra-Hub/refs/heads/main/Required.lua"))() end
 
 if game.PlaceId == 117447183017432 then
-	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/SusyCatV2/Umbra-Hub/refs/heads/main/ElementalConquest.lua"))
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/SusyCatV2/Umbra-Hub/refs/heads/main/ElementalConquest.lua"))()
 end
