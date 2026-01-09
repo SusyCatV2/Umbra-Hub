004febb8d0a524e10d5bc4d863b1bb1a82238a76
Dawg <156109947+SusyCatV2@users.noreply.github.com>
2026-01-09 20:09:43 +0200

    Refactor game.PlaceId check for clarity

--- loader.lua
+++ loader.lua
@@ -1,5 +1,8 @@
 if not _G.umbraLoaded then loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/SusyCatV2/Umbra-Hub/refs/heads/main/Required.lua"))() end
+local id = game.PlaceId
 
-if game.PlaceId == 117447183017432 then
+if id == 117447183017432 then
 	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/SusyCatV2/Umbra-Hub/refs/heads/main/ElementalConquest.lua"))()
+else
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/SusyCatV2/Umbra-Hub/refs/heads/main/ProjectBronzeForever.lua"))()
 end
